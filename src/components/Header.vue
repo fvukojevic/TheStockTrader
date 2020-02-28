e8be6bba2947aacf11738e0a45009c45556f5a28
Ferdo Vukojević <f.vukojevic"globalsoft.ba>
2020-02-28 13:37:27 +0100

    stocks initial setup

---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <router-link to="/" class="navbar-brand">Stock Trader</router-link>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <router-link to="/portfolio" active-class="active" tag="li"><a class="nav-link">Portfolio</a></router-link>
+        <router-link to="/stocks" active-class="active" tag="li"><a class="nav-link">Stocks</a></router-link>
+      </ul>
+      <ul class="navbar-nav navbar-right">
+        <li><a href="#" class="nav-link">End Day</a></li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Save & Load
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <a class="dropdown-item" href="#">Save</a>
+            <a class="dropdown-item" href="#">Load Data</a>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
