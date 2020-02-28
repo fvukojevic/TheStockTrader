cc1564e6653197dc4ff39e499c21f194f342d833
Ferdo Vukojević <f.vukojevic"globalsoft.ba>
2020-02-28 14:52:41 +0100

    revert boostrap version; stocks display;

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,27 +1,28 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <router-link to="/" class="navbar-brand">Stock Trader</router-link>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <router-link to="/" class="navbar-brand">Stock Trader</router-link>
+      </div>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mr-auto">
-        <router-link to="/portfolio" active-class="active" tag="li"><a class="nav-link">Portfolio</a></router-link>
-        <router-link to="/stocks" active-class="active" tag="li"><a class="nav-link">Stocks</a></router-link>
-      </ul>
-      <ul class="navbar-nav navbar-right">
-        <li><a href="#" class="nav-link">End Day</a></li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Save & Load
-          </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <a class="dropdown-item" href="#">Save</a>
-            <a class="dropdown-item" href="#">Load Data</a>
-          </div>
-        </li>
-      </ul>
-    </div>
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav">
+          <router-link to="/portfolio" tag="li" active-class="active"><a>Portfolio</a></router-link>
+          <router-link to="/stocks" tag="li" active-class="active"><a>Stocks</a></router-link>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#">End Day</a></li>
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Save & Load <span class="caret"></span></a>
+            <ul class="dropdown-menu">
+              <li><a href="#">Save Load</a></li>
+              <li><a href="#">Load Data</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
   </nav>
 </template>
--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,3 +1,43 @@
 <template>
-  <h1>The Stocks Stock Component</h1>
+  <div>
+    <app-stock v-for="stock in stocks" v-bind:key="stock.id">
+
+    </app-stock>
+  </div>
 </template>
+
+<script>
+  import Stock from './Stock.vue'
+
+  export default {
+    data() {
+      return {
+        stocks : [
+          {
+            id: 1,
+            name: 'BWM',
+            price: 110,
+          },
+          {
+            id: 2,
+            name: 'Google',
+            price: 200,
+          },
+          {
+            id: 3,
+            name: 'Apple',
+            price: 250,
+          },
+          {
+            id: 4,
+            name: 'Twitter',
+            price: 50,
+          }
+        ]
+      }
+    },
+    components: {
+      appStock: Stock
+    }
+  }
+</script>
